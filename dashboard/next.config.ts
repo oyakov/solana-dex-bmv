d6523e948d4719917831008f7209c72faa965b9a
oyakovlev <oyakovlev@natera.com>
2026-01-13 07:22:41 +0100

    fix: add transpilePackages for d3 compatibility on server

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ["d3", "lucide-react"],
 };
+
 
 export default nextConfig;
